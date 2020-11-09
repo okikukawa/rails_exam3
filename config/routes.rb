767b076317a8f8d5c146e57c32c079e4beeafa89
yosuke kikukawa <kikukawa0131@gmail.com>
2020-11-09 19:10:11 +0900

    rootにrecord/indexを指定

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   resources :sessions, only:[:new, :create, :destroy]
   resources :favorites, only:[:create, :destroy, :index]
   mount LetterOpenerWeb::Engine, at: "/inbox" if Rails.env.development?
+  root to: 'records#index'
 end
